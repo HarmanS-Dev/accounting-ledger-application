d6ca2b1941203bfea6f9abf3cda0d65a03be5b50
harman <harmansingh1099@gmail.com>
2025-10-15 14:56:07 -0400

    Added homeScreen and getTransactionDetails method

--- src/main/java/com/pluralsight/LedgerApplication.java
+++ src/main/java/com/pluralsight/LedgerApplication.java
@@ -12,4 +12,57 @@ public class LedgerApplication {
     public static void main(String[] args) {
 
     }
-}
+
+    private void displayHomeScreen() {
+        String choice;
+        do {
+            System.out.println("""
+                    === Home Screen ===
+                    D) Add Deposit
+                    P) Make Payment (Debit)
+                    L) Ledger
+                    X) Exit""");
+            choice = input.nextLine().toUpperCase();
+
+            switch (choice) {
+                case "D":
+                    addDeposit();
+                    break;
+                case "P":
+                    makePayment();
+                    break;
+                case "L":
+                    displayLedgerScreen();
+                    break;
+                case "X":
+                    System.out.println("Exiting Application. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again");
+            }
+        } while (!choice.equals("X"));
+    }
+
+    private Transactions getTransactionDetails(){
+        System.out.print("Description: ");
+        String description = input.nextLine();
+        System.out.print("Vendor: ");
+        String vendor = input.nextLine();
+        System.out.print("Amount: ");
+        double amount = input.nextDouble();
+
+        return new Transactions(LocalDate.now(), LocalTime.now(), description, vendor, amount);
+    }
+
+    private void addDeposit() {
+
+    }
+
+    private void makePayment() {
+
+    }
+
+    private void displayLedgerScreen(){
+
+    }
+ }
