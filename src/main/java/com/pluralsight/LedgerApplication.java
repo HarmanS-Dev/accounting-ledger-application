7ba55659f5a9b3509aaf864551f132bdc05e3419
harman <harmansingh1099@gmail.com>
2025-10-15 11:33:51 -0400

    first commit

--- src/main/java/com/pluralsight/LedgerApplication.java
+++ src/main/java/com/pluralsight/LedgerApplication.java
@@ -1,6 +1,14 @@
 package com.pluralsight;
 
+import java.io.*;
+import java.time.*;
+import java.util.*;
+
 public class LedgerApplication {
+    private static final String fileName = "transactions.csv";
+    private static final Scanner input = new Scanner(System.in);
+    private static final List<Transactions> transactions = new ArrayList<>();
+
     public static void main(String[] args) {
 
     }
